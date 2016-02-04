1df40dfd7609a5daa04996d5299dd32fd06af4ab
Bradley Few <few.bradley@gmail.com>
2016-02-04 21:33:56 +0000

    Adds package.json and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
+
