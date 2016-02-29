a9372828f419ab029a702cb3e88b17b9c023d845
Bradley Few <few.bradley@gmail.com>
2016-02-29 22:30:54 +0000

    Try with sc universal links association

--- src/btn.js
+++ src/btn.js
@@ -18,7 +18,7 @@ var username    = params['username'].toLowerCase(),
      txt        = document.getElementById('sc-txt');
 
 // Update btn url
-btn.href = "http://bradleyfew.com/scbtn/redirect.html?snapchat_url=snapchat://add/" + username;
+btn.href = "http://bradleyfew.com/scbtn/redirect.html?snapchat_url=https://snapchat.com/add/" + username;
 
 
 if (isLarge) {
