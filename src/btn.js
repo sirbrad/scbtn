002fb1464703c0e08fab8c3529e83cc6f6c1a581
Bradley Few <few.bradley@gmail.com>
2016-02-29 22:38:25 +0000

    test go. url

--- src/btn.js
+++ src/btn.js
@@ -18,7 +18,7 @@ var username    = params['username'].toLowerCase(),
      txt        = document.getElementById('sc-txt');
 
 // Update btn url
-btn.href = "http://bradleyfew.com/scbtn/redirect.html?snapchat_url=https://snapchat.com/add/" + username;
+btn.href = "http://go.snapchat.com/add/" + username;
 
 
 if (isLarge) {
