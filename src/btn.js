87a6c1f3e213b7583ff3b1089b00f32379362a5d
Bradley Few <few.bradley@gmail.com>
2016-04-06 21:09:48 +0100

    Wrap btn.js in an anonymous function. #saynotoglobalscope

--- src/btn.js
+++ src/btn.js
@@ -1,34 +1,37 @@
-var params = (function () {
-  var vars = [],
-      hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-  for (var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
-    vars.push(hash[0]);
-    vars[hash[0]] = hash[1];
+(function(){
+  var params = (function () {
+    var vars = [],
+        hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  }());
+
+  var username    = params['username'].toLowerCase(),
+       isLarge    = (!!params['large'] && params['large'].indexOf('true') >= 0) ? true : false,
+       isInverted = (!!params['invert'] && params['invert'].indexOf('true') >= 0) ? true : false,
+       wrapper    = document.getElementById('sc-wrapper'),
+       btn        = document.getElementById('sc-btn'),
+       txt        = document.getElementById('sc-txt');
+
+  // Update btn url
+  btn.href = "http://snapchat.com/add/" + username;
+  btn.setAttribute('title', 'Add '+username+' on Snapchat')
+
+
+  if (isLarge) {
+    wrapper.className += ' sc-btn--large';
   }
-  return vars;
+
+  if (isInverted) {
+    wrapper.className += ' sc-btn--invert';
+  }
+
+  // Update username text
+  txt.innerHTML = username 
+
 }());
-
-var username    = params['username'].toLowerCase(),
-     isLarge    = (!!params['large'] && params['large'].indexOf('true') >= 0) ? true : false,
-     isInverted = (!!params['invert'] && params['invert'].indexOf('true') >= 0) ? true : false,
-     wrapper    = document.getElementById('sc-wrapper'),
-     btn        = document.getElementById('sc-btn'),
-     txt        = document.getElementById('sc-txt');
-
-// Update btn url
-btn.href = "http://snapchat.com/add/" + username;
-btn.setAttribute('title', 'Add '+username+' on Snapchat')
-
-
-if (isLarge) {
-  wrapper.className += ' sc-btn--large';
-}
-
-if (isInverted) {
-  wrapper.className += ' sc-btn--invert';
-}
-
-// Update username text
-txt.innerHTML = username 
